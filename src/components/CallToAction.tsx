6f5377c35311038768aa9c20a711eb3809ef4d2f
Manish Ahuja <manish.ahuja@bughunters.io>
2025-05-18 20:47:25 +0530

    Updated CallToAction.tsx

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -6,26 +6,6 @@ const CallToAction = () => {
   <div className="absolute -top-20 -left-20 w-64 h-64 bg-white/5 rounded-full blur-3xl"></div>
   <div className="absolute -bottom-40 -right-20 w-80 h-80 bg-white/5 rounded-full blur-3xl"></div>
 
-  {/* Techy Wavy Line SVG */}
-  <svg
-    className="absolute top-0 left-0 w-full h-full opacity-10"
-    viewBox="0 0 1440 320"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    preserveAspectRatio="none"
-  >
-    <path
-      fill="url(#gradient)"
-      fillOpacity="1"
-      d="M0,160L60,138.7C120,117,240,75,360,64C480,53,600,75,720,112C840,149,960,203,1080,213.3C1200,224,1320,192,1380,176L1440,160L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
-    />
-    <defs>
-      <linearGradient id="gradient" x1="0" y1="0" x2="1440" y2="0" gradientUnits="userSpaceOnUse">
-        <stop stopColor="#ffffff" />
-        <stop offset="1" stopColor="#00A8E8" />
-      </linearGradient>
-    </defs>
-  </svg>
 
 
         
