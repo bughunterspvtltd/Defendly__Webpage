5d9db3232ac4b06431051e78e96066ef39758089
Manish Ahuja <manish.ahuja@bughunters.io>
2025-05-18 22:31:17 +0530

    Updated CodeDemo.tsx

--- src/components/CodeDemo.tsx
+++ src/components/CodeDemo.tsx
@@ -76,7 +76,7 @@ const CodeDemo = () => {
                   className="inline-flex items-center bg-gradient-to-r from-[#4285F4] via-[#34A853] to-[#FBBC05] text-white px-8 py-4 rounded-full font-semibold hover:shadow-lg transition-all"
                 >
                   <img 
-                    src="/chrome-icon.png"
+                    src="https://logos.fandom.com/wiki/Chrome_Web_Store?file=Chrome_Web_Store_2022.svg"
                     alt="Chrome Icon"
                     className="w-5 h-5 mr-2"
                   />
